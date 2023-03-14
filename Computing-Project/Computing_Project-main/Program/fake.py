ac2ef7f814bc01b8d49c3a9e8143752de9f93f16
LouisPtrn <patternlouis@gmail.com>
2023-03-14 21:27:12 +0000

    I hate my life 2

--- Computing-Project/Computing_Project-main/Program/fake.py
+++ Computing-Project/Computing_Project-main/Program/fake.py
@@ -1,5 +1,7 @@
-import unittest
+from sys import exit
 from validation import *
+import pygame
+import os
 
 def is_inrange(data, lo, hi):
     try:
@@ -52,29 +54,109 @@ def is_valid_user(u, opt):
     return valid
 
 
-class TestMethods(unittest.TestCase):
-    # -----------------------------------------------------------------------------
-    # TESTING VALIDATION
 
-    # testing dates dd/mm/yyyy format
-    def test_dates(self):
-        self.assertTrue(is_valid_date("01/01/2001"))  # Valid date
-        self.assertFalse(is_valid_date("32/13/2001"))  # Invalid date
-        self.assertFalse(is_valid_date(2))  # Invalid date, wrong type
-        self.assertTrue(is_valid_date("30/12/9999"))  # Extreme (but valid)
 
-    # testing usernames
-    def test_users(self):
-        self.assertTrue(is_valid_user("test_user1", "username"))  # Valid username
-        self.assertFalse(is_valid_user("username_exceeding_chr_limit", "username"))  # Invalid username - too long
-        self.assertFalse(is_valid_user(10, "username"))  # Invalid username - wrong type
+# Spaceship class controlled by the user
+class Player(pygame.sprite.Sprite):
+    def __init__(self, wd, ht):
+        super().__init__()
+        self.wd = wd
+        self.ht = ht
+        self.image_sprite = pygame.image.load("graphics/ship1.png")
+        self.image = pygame.transform.scale(self.image_sprite, (wd/11, ht/11))
+        self.position = pygame.math.Vector2(0, 0)
+        self.rect = self.image.get_rect()
 
-    # testing passwords
-    def test_passwords(self):
-        self.assertTrue(is_valid_user("test_password1", "password"))  # Valid password
-        self.assertFalse(is_valid_user("test", "password"))  # Invalid password - too short
-        self.assertFalse(is_valid_user(True, "password"))  # Invalid password - wrong type
-        self.assertTrue(is_valid_user("test1234", "password"))  # Borderline valid
+    def player_input(self):  # Ship movement from input
+        keys = pygame.key.get_pressed()
+        dx = keys[pygame.K_d] - keys[pygame.K_a]
+        dy = keys[pygame.K_s] - keys[pygame.K_w]
+        self.direction = pygame.math.Vector2(dx, dy)
+        # Accounting for diagonal speed by dividing by root 2
+        if dx != 0 and dy != 0:
+            self.direction /= 1.41421
+
+        # Focus active when shift is held
+        if keys[pygame.K_LSHIFT]:
+            self.position += self.direction * 4
+        else:
+            self.position += self.direction * 8
+
+        # Set rect position to position vector
+        self.rect.x = round(self.position.x)
+        self.rect.y = round(self.position.y)
+
+        if self.rect.left > self.wd:
+            self.rect.right = 0
+        if self.rect.right < 0:
+            self.rect.left = self.wd
+
+        if self.rect.left > self.wd:
+            self.rect.right = 0
+        if self.rect.right < 0:
+            self.rect.left = self.wd
+
+    # def take_dmg1(self):
+    #     self.image = pygame.transform.scale(self.image_sprite, (self.wd/11, self.ht/11))
+    #
+    # def take_dmg2(self):
+    #     self.image = pygame.transform.scale(self.image_inv, (self.wd/11, self.ht/11))
+
+    # def death_check(self, li):
+    #     if li <= 0:
+    #         self.take_dmg2()
+
+    def update(self):
+        self.player_input()
+
+
+def play():
+    play_game = True
+    pygame.init()
+    screen = pygame.display.set_mode((1000, 600))
+    pygame.display.set_caption("Space Game")
+    clock = pygame.time.Clock()
+    player = pygame.sprite.GroupSingle()
+    player.add(Player(1000, 600))
+
+    while play_game:  # Game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        # Update everything
+        screen.fill("black")
+        player.draw(screen)
+        player.update()
+        pygame.display.update()
+        clock.tick(60) # Caps at 60 fps
 
 if __name__ == "__main__":
-    unittest.main()
+    play()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- Computing-Project/Computing_Project-main/Program/testing.py
+++ Computing-Project/Computing_Project-main/Program/testing.py
@@ -11,84 +11,126 @@ from LoginData import *
 from HighscoresData import *
 
 
-class TestMethods(unittest.TestCase):
-    # -----------------------------------------------------------------------------
-    # TESTING SETTINGS
-    def test_invalid_setting(self):  # Tests a setting that doesn't exist
-        self.assertEqual(get_setting("Non-existent setting"), "Setting not found")
-
-    def test_valid_setting(self):  # Testing saving and loading existent settings
-        save_setting("difficulty", "Easy")
-        self.assertEqual(get_setting("difficulty"), "Easy")
-
-        save_setting("difficulty", "Normal")
-        self.assertEqual(get_setting("difficulty"), "Normal")
-
-    def test_colour(self):  # Testing saving and loading colour
-        save_setting("colour", "True")
-        data = (get_setting("colour"))
-        if data == "True":
-            data = 1
-        else:
-            data = 0
-        self.assertTrue(data)
-
-        save_setting("colour", "False")
-        data = (get_setting("colour"))
-        if data == "True":
-            data = 1
-        else:
-            data = 0
-        self.assertFalse(data)
-
-    # -----------------------------------------------------------------------------
-    # TESTING VALIDATION
-    def test_scores(self):
-        self.assertEqual(is_valid_score(1000), True)  # Valid score
-        self.assertEqual(is_valid_score(-100), False)  # Invalid score
-        self.assertEqual(is_valid_score(500.5), False)  # Invalid score
-        self.assertEqual(is_valid_score(0), True)  # Borderline
-        self.assertEqual(is_valid_score(999999), True)  # Borderline
-
-    # testing dates dd/mm/yyyy format
-    def test_dates(self):
-        self.assertEqual(is_valid_date("01/01/2001"), True)  # Valid date
-        self.assertEqual(is_valid_date("29/02/2004"), True)  # Valid date
-        self.assertEqual(is_valid_date("40/10/1000"), False)  # Invalid date
-        self.assertEqual(is_valid_date("31/09/2015"), False)  # Invalid date
-        self.assertEqual(is_valid_date("30/12/9999"), True)  # Extreme
-
-    # testing usernames
-    def test_users(self):
-        self.assertEqual(is_valid_user("Bob_12542", "username"), True)  # Valid username
-        self.assertEqual(is_valid_user("L1", "username"), False)  # Invalid username, too short
-        self.assertEqual(is_valid_user("qwertyuiop_asdfghjklzxc", "username"), False)  # Invalid username, too long
-        self.assertEqual(is_valid_user("qwertyuiop_asgdhjlh", "username"), True)  # Borderline
-
-    def test_passwords(self):
-        self.assertEqual(is_valid_user("XQloP7*jsalHp!", "password"), True)  # Valid password
-        self.assertEqual(is_valid_user("12345", "password"), False)  # Invalid Password
-        self.assertEqual(is_valid_user(10000000000000, "password"), False)  # Invalid Password
+# class TestMethods(unittest.TestCase):
+    # # -----------------------------------------------------------------------------
+    # # TESTING SETTINGS
+    # def test_invalid_setting(self):  # Tests a setting that doesn't exist
+    #     self.assertEqual(get_setting("Non-existent setting"), "Setting not found")
+    #
+    # def test_valid_setting(self):  # Testing saving and loading existent settings
+    #     save_setting("difficulty", "Easy")
+    #     self.assertEqual(get_setting("difficulty"), "Easy")
+    #
+    #     save_setting("difficulty", "Normal")
+    #     self.assertEqual(get_setting("difficulty"), "Normal")
+    #
+    # def test_colour(self):  # Testing saving and loading colour
+    #     save_setting("colour", "True")
+    #     data = (get_setting("colour"))
+    #     if data == "True":
+    #         data = 1
+    #     else:
+    #         data = 0
+    #     self.assertTrue(data)
+    #
+    #     save_setting("colour", "False")
+    #     data = (get_setting("colour"))
+    #     if data == "True":
+    #         data = 1
+    #     else:
+    #         data = 0
+    #     self.assertFalse(data)
+    #
+    # # -----------------------------------------------------------------------------
+    # # TESTING VALIDATION
+    # def test_scores(self):
+    #     self.assertEqual(is_valid_score(1000), True)  # Valid score
+    #     self.assertEqual(is_valid_score(-100), False)  # Invalid score
+    #     self.assertEqual(is_valid_score(500.5), False)  # Invalid score
+    #     self.assertEqual(is_valid_score(0), True)  # Borderline
+    #     self.assertEqual(is_valid_score(999999), True)  # Borderline
+    #
+    # # testing dates dd/mm/yyyy format
+    # def test_dates(self):
+    #     self.assertEqual(is_valid_date("01/01/2001"), True)  # Valid date
+    #     self.assertEqual(is_valid_date("29/02/2004"), True)  # Valid date
+    #     self.assertEqual(is_valid_date("40/10/1000"), False)  # Invalid date
+    #     self.assertEqual(is_valid_date("31/09/2015"), False)  # Invalid date
+    #     self.assertEqual(is_valid_date("30/12/9999"), True)  # Extreme
+    #
+    # # testing usernames
+    # def test_users(self):
+    #     self.assertEqual(is_valid_user("Bob_12542", "username"), True)  # Valid username
+    #     self.assertEqual(is_valid_user("L1", "username"), False)  # Invalid username, too short
+    #     self.assertEqual(is_valid_user("qwertyuiop_asdfghjklzxc", "username"), False)  # Invalid username, too long
+    #     self.assertEqual(is_valid_user("qwertyuiop_asgdhjlh", "username"), True)  # Borderline
+    #
+    # def test_passwords(self):
+    #     self.assertEqual(is_valid_user("XQloP7*jsalHp!", "password"), True)  # Valid password
+    #     self.assertEqual(is_valid_user("12345", "password"), False)  # Invalid Password
+    #     self.assertEqual(is_valid_user(10000000000000, "password"), False)  # Invalid Password
 
     # -----------------------------------------------------------------------------
     # TESTING DB
+    #
+    # def test_existent_user(self):
+    #     enter_user("LouisPattern13", "iloveunittesting")
+    #     self.assertEqual(is_existent_user("LouisPattern13"), True)  # Existent user
+    #     delete_user("LouisPattern13")
+    #     self.assertEqual(is_existent_user("LouisPattern13"), False)  # Non-existent user
+    #
+    # def test_enter_user(self):
+    #     self.assertEqual(enter_user("Inval", "bad"), False)  # Trying to enter invalid data
+    #     self.assertEqual(enter_user("Valid_12345", "ComputerScience015"), True)  # Trying to enter valid data
+    #     delete_user("Valid_12345")
+    #
+    # def test_enter_score(self):
+    #     self.assertEqual(enter_score("User", -100, "Invalid Date"), "Not valid")  # Trying to enter invalid score
+    #     self.assertEqual(enter_score("Test_User", 1000, "01/01/2005"), "Entered successfully")  # Valid score and date
 
-    def test_existent_user(self):
-        enter_user("LouisPattern13", "iloveunittesting")
-        self.assertEqual(is_existent_user("LouisPattern13"), True)  # Existent user
-        delete_user("LouisPattern13")
-        self.assertEqual(is_existent_user("LouisPattern13"), False)  # Non-existent user
 
-    def test_enter_user(self):
-        self.assertEqual(enter_user("Inval", "bad"), False)  # Trying to enter invalid data
-        self.assertEqual(enter_user("Valid_12345", "ComputerScience015"), True)  # Trying to enter valid data
-        delete_user("Valid_12345")
-
-    def test_enter_score(self):
-        self.assertEqual(enter_score("User", -100, "Invalid Date"), "Not valid")  # Trying to enter invalid score
-        self.assertEqual(enter_score("Test_User", 1000, "01/01/2005"), "Entered successfully")  # Valid score and date
     # -----------------------------------------------------------------------------
 
+# class TestMethods(unittest.TestCase):
+#     # -----------------------------------------------------------------------------
+#     # TESTING DB SEARCH
+#     def test_valid_search(self):  # Correct username and password
+#         self.assertFalse(search("test_user1", "testpassword01", "Users"))
+#
+#     def test_false_search(self):
+#         self.assertFalse(search("test_user1", "incorrectpass", "Users")) # Wrong password
+#         self.assertFalse(search("incorrectuser", "testpassword01", "Users"))  # Wrong username
+#         self.assertFalse(search("incorrectuser", "incorrectpass", "Users"))  # Both wrong
+#
+#     def test_invalid_search(self):
+#         self.assertFalse(search("", "", "Users"))
+#         self.assertFalse(search("test_user1", "", "Users"))
+#         self.assertFalse(search("", "testpassword01", "Users"))
+    # -----------------------------------------------------------------------------
+
+class TestMethods(unittest.TestCase):
+    # -----------------------------------------------------------------------------
+    # TESTING VALIDATION
+
+    # testing dates dd/mm/yyyy format
+    def test_dates(self):
+        self.assertTrue(is_valid_date("01/01/2001"))  # Valid date
+        self.assertFalse(is_valid_date("32/13/2001"))  # Invalid date
+        self.assertFalse(is_valid_date(2))  # Invalid date, wrong type
+        self.assertTrue(is_valid_date("30/12/9999"))  # Extreme (but valid)
+
+    # testing usernames
+    def test_users(self):
+        self.assertTrue(is_valid_user("test_user1", "username"))  # Valid username
+        self.assertFalse(is_valid_user("username_exceeding_chr_limit", "username"))  # Invalid username - too long
+        self.assertFalse(is_valid_user(10, "username"))  # Invalid username - wrong type
+
+    # testing passwords
+    def test_passwords(self):
+        self.assertTrue(is_valid_user("test_password1", "password"))  # Valid password
+        self.assertFalse(is_valid_user("test", "password"))  # Invalid password - too short
+        self.assertFalse(is_valid_user(True, "password"))  # Invalid password - wrong type
+        self.assertTrue(is_valid_user("test1234", "password"))  # Borderline valid
 
 if __name__ == '__main__':
     unittest.main()
